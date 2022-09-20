3ca684fed27bf4b2d51a6d2aefb9829c4ef70b01
unknown <ullaskumark29@gmail.com>
2022-09-20 17:37:31 +0530

    Added : Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -2,6 +2,6 @@ package com.bridgelabz.employeewage;
 /*Start with Displaying Welcome to Employee Wage Computation Program on Master Branch*/
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Welcome to Employee Wage Computation Program");
     }
 }
