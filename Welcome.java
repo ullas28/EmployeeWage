a5be52f520889ce7f1e490c1dd22e8229e99a15e
unknown <ullaskumark29@gmail.com>
2022-09-20 14:32:07 +0530

    Added Employee wage

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeewage;
+/*Start with Displaying Welcome to Employee Wage Computation Program on Master Branch*/
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
